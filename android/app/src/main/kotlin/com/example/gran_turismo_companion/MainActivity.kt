9c13b365a57161a8d789ac4c52b866a340ba98ec
Leonardo Vieira <leovieira20@gmail.com>
2020-09-01 13:05:11 +0100

    chore: android stuff

---
+++ android/app/src/main/kotlin/com/example/gran_turismo_companion/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gran_turismo_companion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/grand_turismo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gran_turismo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
